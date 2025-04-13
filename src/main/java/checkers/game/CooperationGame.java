ba071bc0f3636bf1010ad8f480cfef72a1964b63
neyluu <75064225+neyluu@users.noreply.github.com>
2025-04-13 19:34:01 +0200

    Refractored CooperationGame

--- src/main/java/checkers/game/CooperationGame.java
+++ src/main/java/checkers/game/CooperationGame.java
@@ -1,7 +1,5 @@
 package checkers.game;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -12,12 +10,6 @@ public class CooperationGame extends Game
     public CooperationGame()
     {
         super();
-        start();
-    }
-
-    private void start()
-    {
-        // TODO MAN_WHITE will cause problems when King will be added I guess
         turn();
     }
 
@@ -25,46 +17,23 @@ public class CooperationGame extends Game
     {
         System.out.println("Turn start");
 
+        Map<Piece, List<Position[]>> beatMoves = board.getPiecesThatCanBeat(currentTurn);
+        createMoves(beatMoves, true);
+        if(!beatMoves.isEmpty()) return;
 
-        Map<Piece, List<Position[]>> getBeats = board.getPiecesThatCanBeat(currentTurn);
-        for(Map.Entry<Piece, List<Position[]>> entry : getBeats.entrySet())
+        Map<Piece, List<Position[]>> normalMoves = board.getPiecesWithValidMoves(currentTurn);
+        createMoves(normalMoves, false);
+    }
+
+    private void createMoves(Map<Piece, List<Position[]>> movesData, boolean isBeatMoves)
+    {
+        for(Map.Entry<Piece, List<Position[]>> entry : movesData.entrySet())
         {
             Piece piece = entry.getKey();
 
             piece.setOnMouseClicked(e ->
             {
-                clearEvents(piece, getBeats, false);
-
-                List<Position[]> validMoves = entry.getValue();
-                for(Position[] pos : validMoves)
-                {
-                    Cell cell = board.getCell(pos[0].x, pos[0].y);
-                    cell.setStyle("-fx-background-color: rgb(150,41,20);");
-                    cell.setOnMouseClicked(e2 ->
-                    {
-                        board.movePiece(new Position(piece.getX(), piece.getY()), pos[0]);
-                        board.removePiece(pos[1]);
-                        clearEvents(null, getBeats, true);
-
-                        changeTurn();
-                    });
-                }
-            });
-        }
-
-
-        if(!getBeats.isEmpty()) return;
-
-        Map<Piece, List<Position[]>> data = board.getPiecesWithValidMoves(currentTurn);
-        if(data.isEmpty()) System.out.println("Pusto");
-
-        for(Map.Entry<Piece, List<Position[]>> entry : data.entrySet())
-        {
-            Piece piece = entry.getKey();
-//            piece.setStyle("-fx-background-color: rgb(255,0,255);"); //TODO TMP
-            piece.setOnMouseClicked(e ->
-            {
-                clearEvents(piece, data, false);
+                clearEvents(piece, movesData, false);
 
                 List<Position[]> validMoves = entry.getValue();
                 for(Position[] pos : validMoves)
@@ -73,37 +42,18 @@ public class CooperationGame extends Game
                     cell.setStyle("-fx-background-color: rgb(88,41,20);");
                     cell.setOnMouseClicked(e2 ->
                     {
+                        if(isBeatMoves) board.removePiece(pos[1]);
+
                         board.movePiece(new Position(piece.getX(), piece.getY()), pos[0]);
-                        clearEvents(null, data, true);
+                        clearEvents(null, movesData, true);
 
                         changeTurn();
                     });
                 }
-//                piece.setStyle("-fx-background-color: rgb(0,255,255);"); //TODO TMP
             });
         }
     }
 
-//    private void clearEvents(Piece except, Map<Piece, List<Position>> data, boolean clearPiecesEvents)
-//    {
-//        for(Map.Entry<Piece, List<Position>> entry : data.entrySet())
-//        {
-//            if(except != null && entry.getKey() == except) continue;
-//
-//            Piece piece = entry.getKey();
-//            if(piece.isWhite()) piece.setStyle("-fx-background-color: rgb(255,255,255);");
-//            if(piece.isBlack()) piece.setStyle("-fx-background-color: rgb(0,0,0);");
-//
-//            if(clearPiecesEvents) piece.setOnMouseClicked(e -> { } );
-//
-//            List<Position> validMoves = entry.getValue();
-//            for(Position pos : validMoves)
-//            {
-//                board.getCell(pos.x, pos.y).setStyle("-fx-background-color: rgb(128,81,60);");
-//                board.getCell(pos.x, pos.y).setOnMouseClicked(e2 -> { } );
-//            }
-//        }
-//    }
     private void clearEvents(Piece except, Map<Piece, List<Position[]>> data, boolean clearPiecesEvents)
     {
         for(Map.Entry<Piece, List<Position[]>> entry : data.entrySet())
@@ -111,8 +61,8 @@ public class CooperationGame extends Game
             if(except != null && entry.getKey() == except) continue;
 
             Piece piece = entry.getKey();
-            if(piece.isWhite()) piece.setStyle("-fx-background-color: rgb(255,255,255);");
-            if(piece.isBlack()) piece.setStyle("-fx-background-color: rgb(0,0,0);");
+//            if(piece.isWhite()) piece.setStyle("-fx-background-color: rgb(255,255,255);");
+//            if(piece.isBlack()) piece.setStyle("-fx-background-color: rgb(0,0,0);");
 
             if(clearPiecesEvents) piece.setOnMouseClicked(e -> { } );
 
