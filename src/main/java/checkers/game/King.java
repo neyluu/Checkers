85e076352e1bb1276cf018af7e1b03a1b824cae4
neyluu <75064225+neyluu@users.noreply.github.com>
2025-04-17 22:03:00 +0200

    Added beating moves to King

--- src/main/java/checkers/game/King.java
+++ src/main/java/checkers/game/King.java
@@ -24,6 +24,19 @@ public class King extends Piece
         return validMoves;
     }
 
+    @Override
+    public List<Position[]> getBeatMoves(Board board)
+    {
+        List<Position[]> beatMoves = new ArrayList<>();
+
+        beatMoves.addAll(getDiagonalBeatMoves(board, -1, -1));
+        beatMoves.addAll(getDiagonalBeatMoves(board, 1, -1));
+        beatMoves.addAll(getDiagonalBeatMoves(board, -1, 1));
+        beatMoves.addAll(getDiagonalBeatMoves(board, 1, 1));
+
+        return beatMoves;
+    }
+
     private List<Position[]> getDiagonalMoves(Board board, int directionX, int directionY)
     {
         List<Position[]> moves = new ArrayList<>();
@@ -45,10 +58,53 @@ public class King extends Piece
         return moves;
     }
 
-    @Override
-    public List<Position[]> getBeatMoves(Board board)
+    private List<Position[]> getDiagonalBeatMoves(Board board, int directionX, int directionY)
     {
-        return List.of();
-    }
+        List<Position[]> moves = new ArrayList<>();
 
+        Position firstSelf = null;
+        Position firstOther = null;
+        int firstOtherIndex = 1;
+
+        for(int i = 1; i < board.getSize(); i++)
+        {
+            Position pos = new Position(x + i * directionX, y + i * directionY);
+            Cell cell = board.getCell(pos);
+
+            if(board.isInBounds(pos) && cell.havePiece())
+            {
+                if( ! cell.getPiece().isOpposite(this) && firstSelf == null) firstSelf = pos;
+                if(cell.getPiece().isOpposite(this) && firstOther == null)
+                {
+                    firstOther = pos;
+                    firstOtherIndex = i;
+                }
+            }
+            if(firstSelf != null && firstOther != null) break;
+        }
+
+        if(firstOther == null || firstSelf != null &&
+           firstSelf.distanceTo(x, y) < firstOther.distanceTo(x, y))
+        {
+            return List.of();
+        }
+
+        for(int i = firstOtherIndex + 1; i < board.getSize(); i++)
+        {
+            Position pos = new Position(x + i * directionX, y + i * directionY);
+            Cell cell = board.getCell(pos);
+
+            if( ! (board.isInBounds(pos))) break;
+            if(!cell.havePiece())
+            {
+                Position[] positions = new Position[2];
+                positions[0] = pos;
+                positions[1] = firstOther;
+                moves.add(positions);
+            }
+            else break;
+        }
+
+        return moves;
+    }
 }
