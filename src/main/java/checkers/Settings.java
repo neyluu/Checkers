2e2edd5294481a58476204d55ca0745d267119af
neyluu <75064225+neyluu@users.noreply.github.com>
2025-07-28 17:19:27 +0200

    Moved log settings from AppLogger to Settings

--- src/main/java/checkers/Settings.java
+++ src/main/java/checkers/Settings.java
@@ -2,10 +2,30 @@ package checkers;
 
 public class Settings
 {
+    public static class LoggingConsoleFlags
+    {
+        public final boolean error = true;
+        public final boolean warn  = true;
+        public final boolean info  = true;
+        public final boolean debug = true;
+        public final boolean game  = true;
+    }
+    public static class LoggingFileFlags
+    {
+        public final boolean error = true;
+        public final boolean warn  = true;
+        public final boolean info  = true;
+        public final boolean debug = true;
+        public final boolean game  = true;
+    }
+
     public static final int screenWidth = 1280;
     public static final int screenHeight = 720;
 
     public static String iconPath = "/assets/icon.png";
 
     public static boolean logCodeInfo = false;
+
+    public static LoggingConsoleFlags loggingConsole = new LoggingConsoleFlags();
+    public static LoggingFileFlags loggingFile = new LoggingFileFlags();
 }
